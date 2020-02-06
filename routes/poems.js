f63f83fc50bfa1bf79acdc029440cfd751c797fc
unknown <antdwhitehead@gmail.com>
2020-02-06 22:36:34 +0000

    eslint, prettier and poem and cat routes

--- routes/poems.js
+++ routes/poems.js
@@ -1,28 +1,46 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 //Import the mongoose module
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 
 //Set up default mongoose connection
-var mongoDB = 'mongodb://127.0.0.1/geoffrey-whitehead';
+var mongoDB = "mongodb://127.0.0.1/geoffrey_whitehead";
 mongoose.connect(mongoDB, { useNewUrlParser: true });
 
 //Get the default connection
 var db = mongoose.connection;
 
 //Bind connection to error event (to get notification of connection errors)
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 /* GET users listing. */
-router.get('/', function (req, res, next) {
+// router.get("/", function(req, res, next) {
+//   db.collection("poems")
+//     .find()
+//     .toArray(function(err, result) {
+//       if (err) throw err;
+//       res.send(result);
+//     });
+// });
 
-        db.collection('poems').find().toArray(function (err, result) {
-            if (err) throw err
-
-         res.send(result)   
-        })
+router.get("/title/:title", function(req, res, next) {
+  db.collection("poems")
+    .find({ title: req.params.title })
+    .toArray(function(err, result) {
+      if (err) return res.status(500).send("Woops something went wrong");
+      if (!result.length) return res.status(404).send("Poem not found"); 
+      return res.send(result);
+    });
 });
 
+router.get("/category/:cat", function(req, res, next) {
+  db.collection("poems")
+    .find({ category: req.params.cat })
+    .toArray(function(err, result) {
+      if (err) return res.status(500).send("Woops something went wrong");
+      if (!result.length) return res.status(404).send("Category not found") 
+      return res.send(result);
+    });
+})
 module.exports = router;
-
