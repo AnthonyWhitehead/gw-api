329ca318afbcdd112244e7204e22825f36b43fcf
unknown <antdwhitehead@gmail.com>
2020-02-22 22:18:43 +0000

    .

--- src/index.ts
+++ src/index.ts
@@ -32,7 +32,7 @@ app.use(express.json());
 
 app.use("/poems", poemsRouter)
 app.get('*', (req,res) =>{
-    res.sendFile(path.join('/root/gw-client/public/index.html'));
+    res.sendFile('~/gw-client/public/index.html');
 });
 
 /**
